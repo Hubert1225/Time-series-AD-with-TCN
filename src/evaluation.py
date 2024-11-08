fa1e9d75f7321e10428638ab9eaf31231bdfd668
Hubert1225 <hubibaran@gmail.com>
2024-11-08 11:56:51 +0100

    add `get_labels_series` and `F1Score` class in evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+from sklearn.metrics import f1_score
+
 from data_loading import TimeSeriesWithAnoms
 
 
@@ -7,6 +10,8 @@ class EvaluationMetric(ABC):
     """Base class for all metrics for evaluating
     results for subsequence anomaly detection
     """
+
+    name: str
 
     @abstractmethod
     def evaluate(self, ts: TimeSeriesWithAnoms, detected: list[tuple[int, int]]) -> float:
@@ -21,3 +26,26 @@ class EvaluationMetric(ABC):
             metric value
         """
         pass
+
+
+def get_labels_series(length: int, ones_periods: list[tuple[int, int]]) -> np.ndarray:
+    """Returns binary series with ones
+    in desired periods
+    """
+    series = np.zeros(length, dtype=np.uint8)
+    for period_start, period_end in ones_periods:
+        series[period_start:period_end] = 1
+    return series
+
+
+class F1Score(EvaluationMetric):
+
+    name = 'f1-score'
+
+    def evaluate(self, ts: TimeSeriesWithAnoms, detected: list[tuple[int, int]]) -> float:
+        ground_truth_labels = get_labels_series(ts.values.shape[0], ts.annotations)
+        detected_labels = get_labels_series(ts.values.shape[0], detected)
+        return f1_score(
+            y_true=ground_truth_labels,
+            y_pred=detected_labels,
+        )
