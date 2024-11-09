c60a64af0ad8fc8b39b2da3ef24664ad51d88c5a
Hubert1225 <hubibaran@gmail.com>
2024-11-09 16:50:24 +0100

    add utils.py module with `sliding_window` function

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def sliding_window(ts: np.ndarray, window_len: int) -> np.ndarray:
+    """Extracts subsequent sliding windows
+    of given series values
+
+    Args:
+        ts (1D array): series values
+        window_len (int): length of the sliding window
+
+    Returns:
+        2D array: array with subsequent sliding windows,
+            of shape (n_windows, window_len),
+            where n_windows is equal to ts_len - window_len + 1
+    """
+    n_windows = ts.shape[0] - window_len + 1
+
+    sliding_inds = np.tile(np.arange(window_len), (n_windows, 1)) + np.arange(
+        n_windows
+    ).reshape((-1, 1))
+
+    return ts[sliding_inds]
