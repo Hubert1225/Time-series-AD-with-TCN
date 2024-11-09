2d1f1e248e1b931b38abfcb83527b4d81f67e11e
Hubert1225 <hubibaran@gmail.com>
2024-11-09 16:54:52 +0100

    add `nonoverlap_sliding_windows` to utils.py

--- src/utils.py
+++ src/utils.py
@@ -21,3 +21,22 @@ def sliding_window(ts: np.ndarray, window_len: int) -> np.ndarray:
     ).reshape((-1, 1))
 
     return ts[sliding_inds]
+
+
+def nonoverlap_sliding_windows(ts: np.ndarray, window_len: int) -> np.ndarray:
+    """Extract subsequent sliding windows with no overlap
+
+    Args:
+        ts (1D array): series values
+        window_len (int): length of the sliding window
+
+    Returns:
+        2D array: array with subsequent sliding windows,
+            of shape (n_windows, window_len),
+            where n_windows is equal to floor(ts_len / window_len)
+    """
+    n_windows = ts.shape[0] // window_len
+    sliding_inds = np.tile(np.arange(window_len), (n_windows, 1)) + np.arange(
+        0, n_windows * window_len, window_len
+    ).reshape((-1, 1))
+    return ts[sliding_inds]
